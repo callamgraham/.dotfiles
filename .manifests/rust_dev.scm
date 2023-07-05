88b4e5b34850e84be51384f83a5758d4c80dba0f
Callam Graham <callamigraham@gmail.com>
2023-07-05 16:28:19 -0400

    updated the rust script to pass in manifests for packages

---
+++ .manifests/rust_dev.scm
@@ -0,0 +1,15 @@
+(specifications->manifest
+ '(   "bash"
+      "coreutils"
+      "curl"
+      "grep"
+      "nss-certs"
+      "gcc-objc:lib"
+      "gcc-toolchain"
+      "pkg-config"
+      "glib"
+      "git"
+
+      ;; fonts/icons
+      "font-adobe-source-code-pro"
+      ))
