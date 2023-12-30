27d9ab3f364e07a5603eaf4351fdfc658732beea
Callam Graham <callamigraham@gmail.com>
2023-12-30 13:24:55 -0500

    guix home changes: steam, citra, and others

--- .config/guix/home-configuration.scm
+++ .config/guix/home-configuration.scm
@@ -27,12 +27,14 @@
 
 (use-modules (gnu home)
              (gnu packages)
+	     (gnu packages gnupg)
              (gnu services)
              (guix gexp)
 	     (guix channels)
 	     (gnu home services)
              (gnu home services shells)
 	     (gnu home services guix)
+	     (gnu home services gnupg)
 	     (gnu home services ssh))
 
 (home-environment
@@ -42,29 +44,31 @@
   (append (gather-manifest-packages '(emacs))
   (specifications->packages (list
 				      ;; gaming
-				      ;; "steam"
+				      "steam"
+				      "protonup-ng"
 				      "dolphin-emu"
 				      "xrandr" ; for the primary_monitor script
+				      "citra" ; from my personal channel
 				      
 				      ;; sys admin
                                       "parted"
                                       "stow"
 				      "gnupg"
 				      "openssh"
-				      "pinentry"
+				      ;; "pinentry" ;; not needed with the gpg service below
 				      "dunst"
 				      "flatpak"
-				      ;; "nix"
-				      ;; "ntfs-3g" ; for ntfs drives
 				      "zip"
 				      "unzip"
 				      "xdg-user-dirs"
 				      "p7zip"
 				      "neofetch" ; shows user info
 				      "pantalaimon" ; for ement encrypted
+				      "ripgrep"
+				      "rsync"
 				      
 				      ;; browser
-				      "firefox-wayland" ; might want to move this to a container?
+				      "firefox" ; might want to move this to a container?
 
 				      ;; office
 				      "libreoffice"
@@ -98,16 +102,51 @@
 
   ;; Below is the list of Home services.  To search for available
   ;; services, run 'guix home search KEYWORD' in a terminal.
-(services
+ (services
   (list (service home-bash-service-type
                  (home-bash-configuration
-                  (aliases '(("grep" . "grep --color=auto") ("ll" . "ls -l")
-                             ("ls" . "ls -p --color=auto")))
+		  ;; (environment-variables '("PATH" . "$PATH:/home/callam/.bin"))
+                  (aliases '(("grep" . "grep --color=auto")
+			     ("ll" . "ls -l")
+                             ("ls" . "ls -p --color=auto")
+			     ("update-home" . "guix home reconfigure ~/.dotfiles/.config/guix/home-configuration.scm")
+			     ("update-system" . "sudo -E guix system reconfigure /home/callam/.dotfiles/.config/guix/system.scm")
+			     ("cleanup" . "sudo guix gc -d 2m -F 50G")
+			     ("rust" . "cd ~
+guix shell --network --container --emulate-fhs \
+     -m /home/callam/.dotfiles/.manifests/rust_dev.scm \
+     -m /home/callam/.dotfiles/.manifests/emacs.scm \
+     --preserve='^DISPLAY$' \
+     --share=$HOME/Projects/Rust=$HOME \
+     --expose=$HOME/.emacs.d/init.el=$HOME/.emacs.d/init.el \
+     --expose=$HOME/.ssh=$HOME/.ssh \
+     --expose=$HOME/.gnupg=$HOME/.gnupg \
+     --expose=$HOME/.gitconfig=$HOME/.gitconfig \
+     --expose=$HOME/.dotfiles/.bin/rust-shell-init=$HOME/.bin/rust-shell-init \
+     -- ~/.bin/rust-shell-init 
+")
+			     ))
                   ))
-	 ;; setup ssh
-	 (service home-ssh-agent-service-type)
+	;; setup ssh
+	;; (service home-ssh-agent-service-type)
 
-	 (simple-service 'variant-packages-service
+	;; gnupg
+	(service home-gpg-agent-service-type
+		 (home-gpg-agent-configuration
+		  (pinentry-program
+		   (file-append pinentry-emacs "/bin/pinentry-emacs"))
+		  (ssh-support? #t)
+		  (default-cache-ttl 28800)
+		  (max-cache-ttl 28800)
+		  (default-cache-ttl-ssh 28800)
+		  (max-cache-ttl-ssh 28800)
+		  (extra-content
+		   "allow-emacs-pinentry
+allow-loopback-pinentry")
+		  ))
+
+	;; Setup additional channels
+	(simple-service 'variant-packages-service
                 home-channels-service-type
                 (list
                  (channel
@@ -117,12 +156,26 @@
 		   (make-channel-introduction
 		    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
 		    (openpgp-fingerprint
-		     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))))
+		     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+		 (channel
+		  (name 'cig-guix)
+		  (url "https://github.com/callamgraham/cig-guix")
+		  (branch "main")
+		  (introduction
+		   (make-channel-introduction
+		    "ac662eaca8b75b91adbf685904d308a7f34c32c2"
+		    (openpgp-fingerprint
+		     "6DF3 6F47 6B7F 26E1 6861  5C2A 173B 393D E95E D1AE"))))
 
+		 ))
+
+	 ;; setup env variables
 	 (simple-service 'some-useful-env-vars-service
 		home-environment-variables-service-type
-		`(("GUIX_SANDBOX_HOME" . "/extra/nvme1/sandbox")
+		`(("GUIX_SANDBOX_HOME" . "/extra/nvme1/sandbox") ;; used for nonguix containers
+		  ("XDG_DATA_DIRS" . "$XDG_DATA_DIRS:/home/callam/.local/share/flatpak/exports/share") ;; so wofi can launch flatpaks
 		  ("EDITOR" . "emacsclient")
+		  ("PATH" . "$PATH:/home/callam/.bin")
 		  ("XDG_CURRENT_DESKTOP" . "sway")))
 	 
 	 ;; setup dotfiles - this is effecively gnu stow, will need to flesh this out...
@@ -151,8 +204,15 @@
 	 (service home-files-service-type
 		  
 		  `(
-		    (".tmux.conf" ,(local-file "/home/callam/.dotfiles/.tmux.conf"))
-		    (".gitconfig" ,(local-file "/home/callam/.dotfiles/.gitconfig"))))
+		    (".tmux.conf" ,(local-file "/home/callam/.dotfiles/tmux.conf"))
+		    (".gitconfig" ,(local-file "/home/callam/.dotfiles/gitconfig"))
+
+		    ;; emacs
+		    (".emacs.d/init.el" ,(local-file "/home/callam/.dotfiles/.emacs.d/init.el"))
+
+		    ;; gpg superceded by the gpg service above?
+		    ;; (".gnupg/gpg-agent.conf" ,(local-file "/home/callam/.dotfiles/.gnupg/gpg-agent.conf"))
+		    ))
 	 
 	 
 	 ))
