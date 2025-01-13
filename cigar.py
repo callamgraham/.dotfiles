0bc0012a8f5f705e7868288da5f410cd9ff93d01
Callam Graham <callamigraham@gmail.com>
2025-01-12 20:30:45 -0500

    cigar updates - not sure if i'll actually use this...

--- cigar.py
+++ cigar.py
@@ -3,6 +3,61 @@
 import os
 import subprocess
 
+desktop = configuration(
+    pacman_packages = [
+        # system
+        "mesa",
+        "wireplumber",
+        "pipewire",
+        "tailscale",
+
+        #admin,
+        "yazi",
+        "zoxide",
+        "starship",
+        
+
+        #browsers
+        "firefox",
+
+        #editors
+        "emacs-wayland",
+        "helix",
+
+        #window manager/DE
+        "sway",
+        "wofi",
+        "waybar",
+
+        #programming
+        "python",
+        "python-lsp-server",
+        "rustup",
+
+        #gaming,
+        "steam",
+        "retroarch",
+        "libretro-desmume",
+        "libretro-mgba",
+        "libretro-citra",
+        "libretro-pcsx2",
+        "lutris",
+    ],
+    services = [],
+    post_commands = [],
+    symlinks = [],
+    config_files = [],
+)
+
+basement = configuration(
+    pacman_packages = [],
+    services = [],
+    post_commands = [],
+    symlinks = [],
+    config_files = [],
+)
+
+
 class symlink(object):
     def __init__(self, source_file, destination_file):
         self.source_file = source_file
@@ -23,7 +78,7 @@ class configuration(object):
         self.config_files = config_files
 
 class cig(object):
-    def __init__ (self, conf: configuration, computer="Desktop"):
+    def __init__ (self, conf: configuration, computer="desktop"):
         self.conf = configuration
         self.computer = computer
 
@@ -48,11 +103,26 @@ class cig(object):
     def symlinks(self):
         for sym in self.conf.symlinks:
             assert type(sym) == symlink
+
+            # check that the desired parent directories exist
+            dest_directory = os.path.dirname(sym.destination_file)
+            if not os.path.exists(dest_directory):
+                os.makedirs(dest_directory)
+                
+            # create symlink
             os.symlink(sym.source_file, sym.destination_file)
 
     def config_files(self):
         # generates config files from text
-        
+        for config in self.conf.config_file:
+            # check that the desired parent directories exist
+            dest_directory = os.path.dirname(config.file_location)
+            if not os.path.exists(dest_directory):
+                os.makedirs(dest_directory)
+
+            # create file
+            with open(config.file_location, "w") as cfile:
+                cfile.write(config.content)
             
     def sync(self):
         self.sync_pacman()
@@ -63,5 +133,23 @@ class cig(object):
         self.post_commands()
 
         
-            
+if __name__ == "__main__":
+    import sys
+
+    help_text = """
+    Usage:
+    cigar desktop
+    cigar basement
+    """
+    # Check if there are any command-line arguments
+    if len(sys.argv) > 1:
+        # sys.argv[1] is the first argument after the script name
+        if sys.argv[1] == 'desktop':
+            c = cig(desktop, computer="desktop")
+        if sys.argv[1] == 'basement':
+            c = cig(desktop, computer="basement")
+        else:
+            print(help_text)
+    else:
+        print(help_text)
     
